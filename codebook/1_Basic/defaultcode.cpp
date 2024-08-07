75815fc829526a358b51984ba2f2a89db5d2a37e
as6325400 <as6325400@gmail.com>
2024-08-07 16:46:17 +0800

    add default code

---
+++ codebook/1_Basic/defaultcode.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+// #pragma GCC target("popcnt")
+// #pragma GCC optimize("O3")
+using namespace std;
+
+
+void solve() {
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt = 1;
+  cin >> tt;
+  while (t--) {
+      solve();
+  }
+  return 0;
+}
