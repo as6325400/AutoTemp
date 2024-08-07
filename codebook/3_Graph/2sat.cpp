21ecab4fa2151b40523a63d654cfcc207280b187
as6325400 <as6325400@gmail.com>
2024-08-07 16:29:19 +0800

    2sat

---
+++ codebook/3_Graph/2sat.cpp
@@ -0,0 +1,53 @@
+// 用下面的 tarjan scc 算法來解 2 sat 問題，若 事件 a 發生時，事件 b 必然發生，我們須在 a -> b 建立一條有向
+// 用 cses 的 Giant Pizza 來舉例子，給定 n 個人 m 個配料表，每個人可以提兩個要求，兩個要求至少要被滿足一個
+
+// 3 5
+// + 1 + 2
+// - 1 + 3
+// + 4 - 2
+
+// 以這個例子來說，第一個人要求要加 配料1 或者 配料2 其中一項，第二個人要求不要 配料1 或者 要配料3 其中一項
+// 試問能不能滿足所有人的要求，我們可以把 要加配料 i 當作點 i ，不加配料 i 當作點 i + m(配料數量)
+// 關於第一個人的要求 我們可以看成若不加 配料1 則必定要 配料2 以及 若不加 配料2 則必定要 配料1
+// 關於第二個人要求 可看做加了 配料1 就必定要加 配料3 以及 不加 配料3 就必定不加 配料1
+
+// 以這些條件建立有像圖，並且找尋 scc ，若 i 以及 i + m 在同一個 scc 中代表無解
+// 若要求解，則若 i 的 scc_id 小於 i + m 的 scc_id 則 i 為 true ，反之為 false
+// tarjan 在下面
+cin >> n >> m;
+ 
+vector<vector<int>> graph(m * 2 + 1);
+function<int(int)> tr = [&](int x){
+  if(x > m) return x - m;
+  return x + m;
+}; 
+ 
+for(int i = 0; i < n; i++){
+  char c1, c2;
+  int a, b;
+  cin >> c1 >> a >> c2 >> b;
+  // a 代表 a 為真，m + a 代表 a 為假
+  if(c1 == '-') a += m;
+  if(c2 == '-') b += m;
+  graph[tr(a)].push_back(b);
+  graph[tr(b)].push_back(a);
+}
+ 
+tarjan t;
+auto scc = t.scc(graph);
+
+for(int i = 1; i <= m; i++){
+  if(t.scc_id[i] == t.scc_id[tr(i)]){
+    cout << "IMPOSSIBLE\n";
+    return 0;
+  }
+}
+ 
+for(int i = 1; i <= m; i++){
+  if(t.scc_id[i] < t.scc_id[tr(i)]){
+    cout << '+';
+  }
+  else cout << '-';
+  cout << ' ';
+}
+cout << '\n';
