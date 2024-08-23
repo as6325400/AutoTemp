96cd48d2bfe56bb48f90fa748bd127c09fcd4f67
as6325400 <as6325400@gmail.com>
2024-08-23 20:48:44 +0800

    update point

--- codebook/5_Geometry/Point.cpp
+++ codebook/5_Geometry/Point.cpp
@@ -4,60 +4,45 @@ class point{
     T x;
     T y;
     point(){}
-    point(T x, T y){
-        this -> x = x;
-        this -> y = y;
+    point(T _x, T _y){
+        x = _x;
+        y = _y;
     }
-    point<T>& operator+(const point<T> &a);
-    point<T>& operator-(const point<T> &a);
-    point<T>& operator/(const point<T> &a);
-    point<T>& operator/(T a);
-    point<T>& operator*(const T &a);
+    point<T> operator+(const point<T> &a);
+    point<T> operator-(const point<T> &a);
+    point<T> operator/(const point<T> &a);
+    point<T> operator/(T a);
+    point<T> operator*(const T &a);
     bool operator<(const point<T> &a);
 };
 
 template<typename T>
-point<T>& point<T>::operator+(const point<T> &a){
-    auto temp = new point<T>;
-    temp -> x = a.x + this -> x;
-    temp -> y = a.y + this -> y;
-    return *temp;
+point<T> point<T>::operator+(const point<T> &a){
+    return point<T>(x + a.x, y + a.y);
 }
 
 template<typename T>
-point<T>& point<T>::operator-(const point<T> &a){
-    auto temp = new point<T>;
-    temp -> x = this -> x - a.x;
-    temp -> y = this -> y - a.y;
-    return *temp;
+point<T> point<T>::operator-(const point<T> &a){
+    return point<T>(x - a.x, y - a.y);
 }
 
 template<typename T>
-point<T>& point<T>::operator/(const point<T> &a){
-    auto temp = new point<T>;
-    temp -> x = this -> x / a.x;
-    temp -> y = this -> y / a.y;
-    return *temp;
+point<T> point<T>::operator/(const point<T> &a){
+    return point<T>(x / a.x, y / a.y);
 }
 
 template<typename T>
-point<T>& point<T>::operator/(T a){
-    auto temp = new point<T>;
-    temp -> x = this -> x / a;
-    temp -> y = this -> y / a;
-    return *temp;
+point<T> point<T>::operator/(T a){
+    return point<T>(x / a, y / a);
 }
 
 template<typename T>
-point<T>& point<T>::operator*(const T &a){
-    auto temp = new point<T>;
-    temp -> x = this -> x * a;
-    temp -> y = this -> y * a;
-    return *temp;
+point<T> point<T>::operator*(const T &a){
+    return point<T>(x * a, y * a);
 }
 
 template<typename T>
 bool point<T>::operator<(const point<T> &a){
-    if(this -> x != a.x) return this -> x < a.x;
-    return this -> y < a.y;
+    if(x != a.x) return x < a.x;
+    return y < a.y;
 }
