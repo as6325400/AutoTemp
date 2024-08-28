5b85078a6c05b9dc576c94149381d56ecca403f6
as6325400 <as6325400@gmail.com>
2024-08-29 03:58:59 +0800

    update maxflow dicnic

--- codebook/3_Graph/maxflow.cpp
+++ codebook/3_Graph/maxflow.cpp
@@ -1,6 +1,6 @@
 #define int long long
 
-// Edmonds-Karp Algorithm Time: O(VE^2) 實際上會快一點
+// dicnic Algorithm Time: O(V^2E) 實際上會快一點
 // 記得在 main 裡面 resize graph
 // 最小割，找到最少條的邊切除，使得從 src 到 end 的 maxflow 為 0
 // 枚舉所有邊 i -> j ， src 可以到達 i 但無法到達 j ，那這條邊為最小割裡的邊之一
@@ -8,70 +8,63 @@
 class edge{
   public:
     int next;
-    int capicity;
+    int capacity;
     int rev;
     bool is_rev;
-    edge(int _n, int _c, int _r, int _ir) : next(_n), capicity(_c), rev(_r), is_rev(_ir){};
+    edge(int _n, int _c, int _r, int _ir) : next(_n), capacity(_c), rev(_r), is_rev(_ir){};
 };
 
 vector<vector<edge>> graph;
+vector<int> level, iter;
 
 void add_edge(int a, int b, int capacity){
   graph[a].push_back(edge(b, capacity, graph[b].size(), false));
   graph[b].push_back(edge(a, 0, graph[a].size() - 1, true));
 }
 
-int dfs(int now, int end, int flow, vector<pair<int, int>> &path, int idx){
-  if(now == end) return flow;
-  auto &e = graph[now][path[idx + 1].second];
-  if(e.capicity > 0){
-    auto ret = dfs(e.next, end, min(flow, e.capicity), path, idx + 1);
-    if(ret > 0){
-      e.capicity -= ret;
-      graph[e.next][e.rev].capicity += ret;
-      return ret;
+void bfs(int start) {
+  fill(level.begin(), level.end(), -1);
+  queue<int> q;
+  level[start] = 0;
+  q.push(start);
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    for (auto& e : graph[v]) {
+      if (e.capacity > 0 && level[e.next] < 0) {
+        level[e.next] = level[v] + 1;
+        q.push(e.next);
+      }
+    }
+  }
+}
+
+int dfs(int v, int end, int flow) {
+  if (v == end) return flow;
+  for (int &i = iter[v]; i < graph[v].size(); i++) {
+    edge &e = graph[v][i];
+    if (e.capacity > 0 && level[v] < level[e.next]) {
+      int d = dfs(e.next, end, min(flow, e.capacity));
+      if (d > 0) {
+        e.capacity -= d;
+        graph[e.next][e.rev].capacity += d;
+        return d;
+      }
     }
   }
   return 0;
 }
 
-vector<pair<int, int>> search_path(int start, int end){
-  vector<pair<int, int>> ans;
-  queue<int> q;
-  vector<pair<int, int>> parent(graph.size(), {-1, -1});
-  q.push(start);
-  while(!q.empty()){
-    int now = q.front();
-    q.pop();
-    for(int i = 0; i < (int)graph[now].size(); i++){
-      auto &e = graph[now][i];
-      if(e.capicity > 0 and parent[e.next].first == -1){
-        parent[e.next] = {now, i};
-        if(e.next == end) break;
-        q.push(e.next);
-      }
+int maxflow(int start, int end) {
+  int flow = 0;
+  level.resize(graph.size() + 1);
+  while (true) {
+    bfs(start);
+    if (level[end] < 0) return flow;
+    iter.assign(graph.size() + 1, 0);
+    int f;
+    while ((f = dfs(start, end, 1e9)) > 0) {
+      flow += f;
     }
   }
-  if(parent[end].first == -1) return ans;
-  int now = end;
-  while(now != start){
-    auto [node, idx] = parent[now];
-    ans.emplace_back(node, idx);
-    now = node;
-  }
-  ans.emplace_back(start, -1);
-  reverse(ans.begin(), ans.end());
-  return ans;
-}
-
-int maxflow(int start, int end){
-  int ans = 0;
-  while(1){
-    vector<bool> visited(graph.size() + 1, false);
-    auto tmp = search_path(start, end);
-    if(tmp.size() == 0) break;
-    auto flow = dfs(start, end, 1e9, tmp, 0);
-    ans += flow;
-  }
-  return ans;
 }
