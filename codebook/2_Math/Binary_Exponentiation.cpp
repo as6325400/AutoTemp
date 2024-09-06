548d771e1a469b2065b76b9a481cf68a2860483a
as6325400 <as6325400@gmail.com>
2024-09-06 19:28:07 +0800

    update fast pow

--- codebook/2_Math/Binary_Exponentiation.cpp
+++ codebook/2_Math/Binary_Exponentiation.cpp
@@ -1,6 +1,7 @@
 #define int long long
 
 // 根據費馬小定理，若 a p 互質，a^(p-2) 為 a 在 mod p 時的乘法逆元
+// a ^ (b ^ c) % mod = fast_pow(a, fast_pow(b, c, mod - 1), mod)
 typedef unsigned long long ull;
 inline int ksc(ull x, ull y, int p) {  // O(1)快速乘（防爆long long）
   return (x * y - (ull)((long double)x / p * y) * p + p) % p;
