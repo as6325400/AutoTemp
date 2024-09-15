e756d9124d9611e6b9bd5491ba57fd6008a791e9
MixnightE <mixnight810412@gmail.com>
2024-09-15 16:45:39 +0800

    modify default code.

--- codebook/1_Basic/defaultcode.cpp
+++ codebook/1_Basic/defaultcode.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 #define int long long
+#define endl '\n' // 如果是互動題要把這個註解掉
 // #pragma GCC target("popcnt")
 // #pragma GCC optimize("O3")
 using namespace std;
+int tt = 1;
 
-
-void solve() {
-
+void pre() {
+  cout.tie(nullptr); // 輸出加速
+  cin >> tt; // 多筆輸入
 }
+
+void solve() {}
 
 signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  int tt = 1;
-  cin >> tt;
-  while (t--) {
-      solve();
-  }
+#ifdef LOCAL
+  // g++ -DLOCAL -std=c++17 <filename> && ./a.out
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif // LOCAL
+  pre();
+  while (tt--) { solve(); }
   return 0;
 }
