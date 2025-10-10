69dfb7a0a97f714cd51f885646f3734266505fc0
as6325400 <as6325400@gmail.com>
2025-10-11 04:25:36 +0800

    HLD

---
+++ codebook/9_Tree/HLD.cpp
@@ -0,0 +1,81 @@
+struct HLD {
+    int n, cur;
+    vector<int> siz, top, dep, parent, in, out, seq;
+    vector<vector<int>> adj;
+    HLD(int n) : n(n), cur(0) {
+        siz.resize(n); top.resize(n); dep.resize(n);
+        parent.resize(n); in.resize(n); out.resize(n);
+        seq.resize(n); adj.assign(n, {});
+    }
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    void work(int rt = 0) {
+        top[rt] = rt;
+        dep[rt] = 0;
+        parent[rt] = -1;
+        dfs1(rt); dfs2(rt);
+    }
+    void dfs1(int u) {
+        if (parent[u] != -1)
+            adj[u].erase(find(adj[u].begin(), adj[u].end(), parent[u]));
+        siz[u] = 1;
+        for (auto &v : adj[u]) {
+            parent[v] = u, dep[v] = dep[u] + 1;
+            dfs1(v);
+            siz[u] += siz[v];
+            if (siz[v] > siz[adj[u][0]]) {
+                swap(v, adj[u][0]);
+            } // 讓 adj[u][0] 是重子節點
+        }
+    }
+    void dfs2(int u) {
+        in[u] = cur++;
+        seq[in[u]] = u; // dfn 對應的編號
+        for (auto v : adj[u]) {
+            top[v] = v == adj[u][0] ? top[u] : v;
+            dfs2(v);
+        }
+        out[u] = cur;
+    }
+    int lca(int u, int v) {
+        while (top[u] != top[v]) {
+            if (dep[top[u]] > dep[top[v]]) {
+                u = parent[top[u]];
+            } else {
+                v = parent[top[v]];
+            }
+        }
+        return dep[u] < dep[v] ? u : v;
+    }
+    int dist(int u, int v) {
+        return dep[u] + dep[v] - 2 * dep[lca(u, v)];
+    }
+    int jump(int u, int k) {
+        if (dep[u] < k) return -1;
+        int d = dep[u] - k;
+        while (dep[top[u]] > d) u = parent[top[u]];
+        return seq[in[u] - dep[u] + d];
+    }
+    bool isAncester(int u, int v) {
+        return in[u] <= in[v] && in[v] < out[u];
+    }
+    int rootedParent(int rt, int v) {
+        if (rt == v) return rt;
+        if (!isAncester(v, rt)) return parent[v];
+        auto it = upper_bound(adj[v].begin(), adj[v].end(), rt,
+            [&](int x, int y) {
+                return in[x] < in[y];
+            }) - 1;
+        return *it;
+    }
+    int rootedSize(int rt, int v) {
+        if (rt == v) return n;
+        if (!isAncester(v, rt)) return siz[v];
+        return n - siz[rootedParent(rt, v)];
+    }
+    int rootedLca(int rt, int a, int b) {
+        return lca(rt, a) ^ lca(a, b) ^ lca(b, rt);
+    }
+};
