8c3c05f4b70a59e250424ce79bc4ac804b9d6c85
LO-WEN-CHIEN <nandokabeam@gmail.com>
2025-10-11 00:03:34 +0800

    add point segment distance

--- codebook/5_Geometry/point_calculate.cpp
+++ codebook/5_Geometry/point_calculate.cpp
@@ -12,3 +12,16 @@ template<typename T>
 T len(point<T> p){
     return sqrt(dot(p, p));
 }
+
+template<typename T>
+int sign(T x){
+    return x == 0 ? 0 : x > 0 ? 1 : -1;;
+}
+
+template<typename T>
+T pointSegDist(point<T> q0, point<T> q1, point<T> p) {
+    if (sign(len(q0 - q1)) == 0) return len(q0 - p);
+    if (sign(dot(q1 - q0, p - q0)) >= 0 && sign(dot(q0 - q1, p - q1)) >= 0)
+        return abs(cross(q1 - q0, p - q0) / len(q0 - q1));
+    return min(len(p - q0), len(p - q1));
+}
